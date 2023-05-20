8f58498049b7f830a5a3056ad3747e2b9bc594b2
Nathan Wiles <nathanawiles@gmail.com>
2023-05-20 21:38:42 +0000

    add functions from previous assignments to without.js

---
+++ without.js
@@ -0,0 +1,42 @@
+// eqArrays() function from previous assignment
+const eqArrays  = function(array1, array2) {
+  // set default output to false;
+  let output = false;
+  // check if array lengths are the same.
+  if (array1.length === array2.length) {
+    // if length is equal, set output to true.
+    output = true;
+    // check if array element are not the same.
+    for (let i = 0; i < array1.length; i++) {
+      // if any array element is not the same, change output to false, and break the loop.
+      if (array1[i] !== array2[i]) {
+        output = false;
+        break;
+      }
+    }
+  }
+  // return output aka true or false depending on arrays passed
+  return output;
+  
+};
+
+// assertArraysEqual() function from previous assignment.
+const assertArraysEqual = function(array1, array2) {
+  // use eqArray to find if arrays are equal. assign return value to equalityBool
+  let equalityBool = eqArrays(array1, array2);
+  // check if equality bool is true
+  if (equalityBool) {
+    // if true log passed message
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    // else log failed message
+    console.log(`🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`);
+
+    // log additional context
+    if (array1.toString() === array2.toString()) {
+      console.log('↑↑↑↑↑↑ Reason: Mismatched data types');
+    } else {
+      console.log('↑↑↑↑↑↑ Reason: Array Values not equal');
+    }
+  }
+};
