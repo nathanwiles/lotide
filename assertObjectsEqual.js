c479d855ecb39b208020fad7c20f20d8cc8a0fee
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-05-26 23:52:04 -0400

    create, solve

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,18 +1,19 @@
 // DEFINE HELPER FUNCTIONS
 // define assertEqual function
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   let resultingBool = actual === expected;
   resultingBool
     ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
     : console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
 };
 // define eqArrays function
-const eqArrays  = function(array1, array2) {
+const eqArrays = function (array1, array2) {
   // if arrays are different length return false.
   if (array1.length !== array2.length) return false;
   for (let i = 0; i < array1.length; i++) {
     // if any array element is not the same, return false.
-    if (array1[i] !== array2[i]) { // check if array element are not the same.
+    if (array1[i] !== array2[i]) {
+      // check if array element are not the same.
       return false;
     }
   }
@@ -20,17 +21,33 @@ const eqArrays  = function(array1, array2) {
   return true;
 };
 // Define eqObjects function
-const eqObjects = function(obj1, obj2) {
+const eqObjects = function (obj1, obj2) {
   const obj1Keys = Object.keys(obj1);
   const obj2Keys = Object.keys(obj2);
   if (obj1Keys.length !== obj2Keys.length) return false;
   for (const key of obj1Keys) {
     let value1 = obj1[key];
     let value2 = obj2[key];
-    
+
     if (Array.isArray(value1)) {
-      if (eqArrays(value1, value2) !== true) return false;
+      if (!(eqArrays(value1, value2))) return false;
     } else if (obj1[key] !== obj2[key]) return false;
   }
   return true; // if the code makes it this far all test were passed.
 };
+// MAIN FUNCTION IMPLEMENTATION
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require('util').inspect;
+  eqObjects(actual, expected)
+    ? console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`)
+    : console.log(`🔴🔴🔴 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+};
+
+// TEST CASES
+
+let obj1 = {num:43,str:'hjhj',arr:[1,2,3,4]};
+let obj2 = {num:43,str:'hjhj',arr:[1,2,3,4]};
+let obj3 = {num:43,str:'nope',arr:[1,2,3,4]};
+
+assertObjectsEqual(obj1, obj2);
+assertObjectsEqual(obj3, obj2);
