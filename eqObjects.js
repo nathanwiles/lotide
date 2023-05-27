c167d5a84333e4b18af799fd9fec773692f5564b
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-05-26 23:20:38 -0400

    eqObjects.js: build eqArrays into eqObjects()

--- eqObjects.js
+++ eqObjects.js
@@ -1,9 +1,24 @@
-// DEFINE HELPER FUNCTION
+// DEFINE HELPER FUNCTIONS
+// define 
 const assertEqual = function (actual, expected) {
   let resultingBool = actual === expected;
   resultingBool
     ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
     : console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// define eqArrays function
+const eqArrays  = function(array1, array2) {
+  // if arrays are different length return false.
+  if (array1.length !== array2.length) return false;
+  for (let i = 0; i < array1.length; i++) {
+    // if any array element is not the same, return false.
+    if (array1[i] !== array2[i]) { // check if array element are not the same.
+      return false;
+    }
+  }
+  // all tests passed if reached.
+  return true;
 };
 
 /* TASK:
@@ -26,7 +41,12 @@ const eqObjects = function (obj1, obj2) {
   const obj2Keys = Object.keys(obj2);
   if (obj1Keys.length !== obj2Keys.length) return false;
   for (const key of obj1Keys) {
-    if (obj1[key] !== obj2[key]) return false;
+    let value1 = obj1[key];
+    let value2 = obj2[key];
+    
+    if (Array.isArray(value1)) {
+      if (eqArrays(value1, value2) !== true) return false;
+    } else if (obj1[key] !== obj2[key]) return false;
   }
   return true; // if the code makes it this far all test were passed.
 };
@@ -41,6 +61,7 @@ assertEqual(eqObjects(longPantsObject, pantsObject), false); // => false
 
 const multiColourPantsObject = { colour: ["blue", "green"], size: 32 };
 const anotherMultiColourPantsObject = { size: 32, colour: ["blue", "green"] };
+
 assertEqual(
   eqObjects(multiColourPantsObject, anotherMultiColourPantsObject),
   true
@@ -54,6 +75,6 @@ const longMultiColourPantsObject = {
 };
 
 assertEqual(
-  eqObjects(multiColourPantsObject, anotherMultiColourPantsObject),
+  eqObjects(multiColourPantsObject, longMultiColourPantsObject),
   false
 );
