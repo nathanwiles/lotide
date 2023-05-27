574e3cb2e1d488de37c3cc3bab1b46010556f85d
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-05-26 21:59:54 -0400

    eqObjects.js: Add test cases for array values.

--- eqObjects.js
+++ eqObjects.js
@@ -1,7 +1,6 @@
 // DEFINE HELPER FUNCTION
 const assertEqual = function (actual, expected) {
-  
-  let resultingBool = (actual === expected);
+  let resultingBool = actual === expected;
   resultingBool
     ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
     : console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
@@ -11,30 +10,50 @@ const assertEqual = function (actual, expected) {
   - DEFINE eqObjects:
     -take in 2 objects as arguments
     -check if both have the same number of keys
-      -if false return false
-      -if true:
-      -iterate over the first Object's keys
-        -check if values, referenced by the key, match using assertEqual()
-        -
+      -if not return false
+    -iterate over the first Object's keys
+      -check if values match each other
+        -if not return false.
+      
+    return true as tests are passed.
+
             
 */
 
+// Define eqObjects function
 const eqObjects = function (obj1, obj2) {
-  //const obj1Keys = Object.keys(obj1);
-  //const obj2Keys = Object.keys(obj2);
-  //if (assertEqual(obj1Keys.length, obj2Keys.length, "Quantiny of keys:")) {
-    
-
-  //};
+  const obj1Keys = Object.keys(obj1);
+  const obj2Keys = Object.keys(obj2);
+  if (obj1Keys.length !== obj2Keys.length) return false;
+  for (const key of obj1Keys) {
+    if (obj1[key] !== obj2[key]) return false;
+  }
+  return true; // if the code makes it this far all test were passed.
 };
 
-
-// TEST CASES
-const pantsObject = {colour: "blue", size: 32};
-const anotherPantsObject = {size: 32, colour: "blue"};
+// TEST CASES: Primitive values
+const pantsObject = { colour: "blue", size: 32 };
+const anotherPantsObject = { size: 32, colour: "blue" };
 assertEqual(eqObjects(pantsObject, anotherPantsObject), true); // => true
 
-const longPantsObject = {colour: "blue", size: 32, length: 34};
+const longPantsObject = { colour: "blue", size: 32, length: 34 };
 assertEqual(eqObjects(longPantsObject, pantsObject), false); // => false
 
+const multiColourPantsObject = { colour: ["blue", "green"], size: 32 };
+const anotherMultiColourPantsObject = { size: 32, colour: ["blue", "green"] };
+assertEqual(
+  eqObjects(multiColourPantsObject, anotherMultiColourPantsObject),
+  true
+);
 
+// TEST CASES: Array values
+const longMultiColourPantsObject = {
+  size: 32,
+  colour: ["blue", "green"],
+  length: 34,
+};
+
+assertEqual(
+  eqObjects(multiColourPantsObject, anotherMultiColourPantsObject),
+  false
+);
