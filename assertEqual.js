2db95a0e7ab8b5b853a55723b84f427ab8b92e3b
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-05-28 21:15:32 -0400

    quick lint

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,11 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  (actual === expected) ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) :
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-
+  actual === expected
+    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
 };
 // TEST CODE
 assertEqual("Tidal waves", "Ocean");
-assertEqual(6,6);
+assertEqual(6, 6);
 assertEqual("bologna", "bologna");
-assertEqual(4,7);
+assertEqual(4, 7);
