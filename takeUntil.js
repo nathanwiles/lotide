ec4da8159a59e62d04b6cb9deb9c811cae57ce20
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-05-28 21:05:57 -0400

    lint

--- takeUntil.js
+++ takeUntil.js
@@ -1,5 +1,5 @@
 // eqArrays function
-const eqArrays  = function(array1, array2) {
+const eqArrays = function(array1, array2) {
   // set default output to false;
   let output = false;
   // check if array lengths are the same.
@@ -8,8 +8,8 @@ const eqArrays  = function(array1, array2) {
     output = true;
     // check if array element are not the same.
     for (let i = 0; i < array1.length; i++) {
-       // handles nested arrays.
-       if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      // handles nested arrays.
+      if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
         if (!eqArrays(array1[i], array2[i])) {
           output = false;
           break;
@@ -23,7 +23,6 @@ const eqArrays  = function(array1, array2) {
   }
   // return output aka true or false depending on arrays passed
   return output;
-  
 };
 // assertArraysEqual()
 const assertArraysEqual = function(array1, array2) {
@@ -39,14 +38,12 @@ const assertArraysEqual = function(array1, array2) {
 
     // log additional context
     if (array1.toString() === array2.toString()) {
-      console.log('↑↑↑↑↑↑ Reason: Mismatched data types');
+      console.log("↑↑↑↑↑↑ Reason: Mismatched data types");
     } else {
-      console.log('↑↑↑↑↑↑ Reason: Array Values not equal');
+      console.log("↑↑↑↑↑↑ Reason: Array Values not equal");
     }
   }
 };
-
-
 
 // Main Function
 const takeUntil = (array, cb) => {
@@ -61,15 +58,14 @@ const takeUntil = (array, cb) => {
   return returnArray;
 };
 
-
 // TEST CASES
 
-const data1 = [3,6,9,12,15,18,21];
+const data1 = [3, 6, 9, 12, 15, 18, 21];
 const results1 = takeUntil(data1, (x) => x > 10);
-assertArraysEqual(results1,[3,6,9]);
+assertArraysEqual(results1, [3, 6, 9]);
 
 console.log("---");
 
-const data2 = ["take","me","to","the","place","I","love",];
+const data2 = ["take", "me", "to", "the", "place", "I", "love"];
 const results2 = takeUntil(data2, (x) => x === "to");
-assertArraysEqual(results2, ["take","me"]);
+assertArraysEqual(results2, ["take", "me"]);
