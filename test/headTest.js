cf4a4cc33a932880ca6d4b7c5b57dfb214fdf03e
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-10 00:24:51 -0400

    rework to use mocha chai assertions.

--- test/headTest.js
+++ test/headTest.js
@@ -1,12 +1,23 @@
 // test code for head.js
 // pull required functions from head.js and assertEqual.js
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
+describe("#head", () => {
+  it("returns 1 for ['1,2,3]'", () => {
+    assert.equal(head([1,2,3]), 1);
+  });
+  it("returns 'Hello' for '['Hello', 'Lighthouse', 'Labs']'", () => {
+    assert.equal(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+  it("returns 1 for '[1]'", () => {
+    assert.equal(head([1]), 1);
+  });
+  it("returns undefined for []", () => {
+    assert.isUndefined(head([]));
+  });
+  
+});
 
-assertEqual(head([5,6,7]), 3);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([1]), 1);
-assertEqual(head([]), undefined);
 
 
