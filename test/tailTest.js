6901a667ddf9a02f69b49d087e245f2160990252
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-06-10 00:32:24 -0400

    use Chai asserts

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,25 +1,19 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-
-
-// Test Case: Check tail() output values
-const result = tail(["first", "second", "third"]);
-assertEqual(result.length, 2); // check for proper length
-assertEqual(result[0], "second"); // ensure first element is now "second"
-assertEqual(result[1], "third"); // ensure second element is now "third"
-
-// Test Case: Check the original array
-const words = ["first", "second", "third"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// Test Case: Check output from single value array
-const result2 = tail(['solo']);
-assertEqual(result2.length, 0); // check for proper length
-assertEqual(result2[0], undefined); // ensure result is empty
-
-// Test Case: Check output from empty array
-const result3 = tail([]);
-assertEqual(result3.length, 0); // check for proper length
-assertEqual(result3[0], undefined);
+describe("#tail", () => {
+  it("returns ['second', 'third'] for ['first', 'second', 'third']", () => {
+    assert.deepEqual(tail(['first', 'second', 'third']), ['second', 'third']);
+  });
+  it("should not modify the original array", () => {
+    const words = ["first", "second", "third"];
+    tail(words);
+    assert.deepEqual(words, ["first", "second", "third"]);
+  });
+  it("returns [] for ['solo']", () => {
+    assert.deepEqual(tail(['solo']), []);
+  });
+  it("returns [] for []", () => {
+    assert.isEmpty(tail([]));
+  });
+});
